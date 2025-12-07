e5dcb6f0621195cec8393007f16769247060b35e
Will Train <87541128+wt-314159@users.noreply.github.com>
2025-12-07 15:29:52 +0000

    day7 part two complete

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let input = include_str!("../puzzle_input.txt");
 
+    #[allow(unused_variables)]
     let test_input = r".......S.......
 ...............
 .......^.......
@@ -18,10 +19,13 @@ fn main() {
 .^.^.^.^.^...^.
 ...............";
 
-    let split_count = count_splits(input);
-    println!("Split count: {split_count}");
+    // let split_count = count_splits(input);
+    // println!("Split count: {split_count}");
+    let paths_count = count_pathways(input);
+    println!("Number of paths: {paths_count}");
 }
 
+#[allow(dead_code)]
 fn count_splits(input: &str) -> usize {
     let mut total = 0;
     let mut input_lines = input.lines().peekable();
@@ -44,4 +48,26 @@ fn count_splits(input: &str) -> usize {
         }
     }
     total
+}
+
+fn count_pathways(input: &str) -> usize {
+    let mut input_lines = input.lines();
+    let first_line = input_lines.next().expect("No lines!");
+    let mut paths = vec![0; first_line.len()];
+    let beam_idx = first_line.find('S').expect("No 'S' on first line");
+    paths[beam_idx] = 1;
+
+    // We can skip 2nd line, and skip every alternate line
+    for line in input_lines.skip(1).step_by(2) {
+        for (i, c) in line.chars().enumerate() {
+            // If an existing beam hits a splitter...
+            if c == '^' && paths[i] > 0 {
+                // ...add number of paths to either side, and set num paths to 0
+                paths[i - 1] += paths[i];
+                paths[i + 1] += paths[i];
+                paths[i] = 0;
+            }
+        }
+    }
+    paths.iter().sum()
 }
