280e0c82874d6380fdc9192e191e43ba05929e12
Will Train <87541128+wt-314159@users.noreply.github.com>
2025-12-09 22:01:01 +0000

    Changing approach, still work in progress

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -30,29 +30,47 @@ fn main() {
 
     let positions = get_positions(test_input);
     let closest_pairs = find_closest_pairs(&positions);
-    // for pair in closest_pairs.iter().take(10) {
-    //     println!(
-    //         "{:?} -> {:?} = {}",
-    //         positions[pair.1], positions[pair.2], pair.0
-    //     );
-    // }
-    let circuits = group_by_closest(&closest_pairs);
-    for take_five in circuits.iter() {
+    for pair in closest_pairs.iter() {
         println!(
-            "Circuit size: {}",
-            take_five.as_ref().map_or(0, |v| v.len())
+            "{:?} -> {:?} = {}, simple dist. {}",
+            positions[pair.1],
+            positions[pair.2],
+            pair.0,
+            dumb_distance(&positions[pair.1], &positions[pair.2])
         );
     }
+    let circuits = group_by_closest(&closest_pairs, 10);
+    for take_five in circuits.iter() {
+        println!("Circuit size: {}", take_five.len());
+        // for position in take_five.as_ref().unwrap() {
+        //     println!("\t{:?}", position);
+        // }
+    }
+}
+
+fn dumb_distance(pos1: &Position, pos2: &Position) -> f64 {
+    (pos1.x - pos2.x).abs() + (pos1.y - pos2.y).abs() + (pos1.z - pos2.z).abs()
 }
 
 fn find_closest_pairs(positions: &[Position]) -> Vec<(f64, usize, usize)> {
     let len = positions.len();
-    let mut pair_distances = Vec::with_capacity(len * (len - 1));
+    let mut used = HashSet::new();
+    let mut pair_distances = Vec::with_capacity(len);
     for i in 0..len {
-        for j in i + 1..len {
-            let distance = positions[i].distance_from(&positions[j]);
-            pair_distances.push((distance, i, j));
+        // we've already found closest pair for this idx
+        if used.contains(&i) {
+            continue;
         }
+        let (min_idx, min) = positions
+            .iter()
+            .enumerate()
+            .filter(|(idx, _)| *idx != i)
+            .map(|(idx, pos)| (idx, pos.distance_from(&positions[i])))
+            .min_by(|a, b| a.1.total_cmp(&b.1))
+            .expect("Failed to find minimum");
+        pair_distances.push((min, i, min_idx));
+        used.insert(i);
+        used.insert(min_idx);
     }
     pair_distances.sort_by(|a, b| a.0.total_cmp(&b.0));
     pair_distances
@@ -62,69 +80,50 @@ fn get_positions(input: &str) -> Vec<Position> {
     input.lines().map(Position::from).collect()
 }
 
-fn group_by_closest(closest_pairs: &[(f64, usize, usize)]) -> Vec<Option<HashSet<usize>>> {
-    let mut circuits: Vec<Option<HashSet<usize>>> = Vec::new();
-    let mut map: HashMap<usize, usize> = HashMap::new();
-    for pair in closest_pairs {
-        match (map.get(&pair.1), map.get(&pair.2)) {
-            (None, None) => {
-                let mut new_circuit = HashSet::new();
-                new_circuit.insert(pair.1);
-                new_circuit.insert(pair.2);
-                circuits.push(Some(new_circuit));
-                map.insert(pair.1, circuits.len() - 1);
-                map.insert(pair.2, circuits.len() - 1);
-            }
-            (Some(&idx), None) => {
-                let mut circuit = circuits[idx].take().expect("No circuit found!");
-                circuit.insert(pair.2);
-                circuits[idx] = Some(circuit);
-                map.insert(pair.2, idx);
-            }
-            (None, Some(&idx)) => {
-                let mut circuit = circuits[idx].take().expect("No circuit found");
-                circuit.insert(pair.1);
-                circuits[idx] = Some(circuit);
-                map.insert(pair.1, idx);
-            }
-            (Some(idx1), Some(idx2)) if idx1 == idx2 => {
-                // do nothing, already in the same circuit
-            }
-            (Some(&idx1), Some(&idx2)) => {
-                // both junctions are already in separate circuits,
-                // merge circuits into one
-                let mut circuit;
-                let (idx_use, idx_drop, pair_add);
-                // eprintln!(
-                //     "Indices: {}, {}; circuits.len(): {}; pair: {} -> {}",
-                //     idx1,
-                //     idx2,
-                //     circuits.len(),
-                //     pair.1,
-                //     pair.2,
-                // );
-                let circuit1 = circuits[idx1].take().expect("No circuit at idx1");
-                let circuit2 = circuits[idx2].take().expect("No circuit at idx2");
-                if circuit1.len() > circuit2.len() {
-                    circuit = circuit1;
-                    (idx_use, idx_drop, pair_add) = (idx1, idx2, pair.2);
-                } else {
-                    circuit = circuit2;
-                    (idx_use, idx_drop, pair_add) = (idx2, idx1, pair.1);
+fn group_by_closest_two(closest_pairs: &[(f64, usize, usize)], num_connections: usize) {}
+
+fn group_by_closest(
+    closest_pairs: &[(f64, usize, usize)],
+    num_connections: usize,
+) -> Vec<HashSet<usize>> {
+    let mut circuits = Vec::new();
+    let mut used = HashSet::new();
+    let mut connection_count = 0;
+    'outer: while let Some(start_pair) = closest_pairs.iter().find(|p| !used.contains(&p.1)) {
+        let mut circuit = HashSet::new();
+        circuit.insert(start_pair.1);
+        circuit.insert(start_pair.2);
+        used.insert(start_pair.1);
+        used.insert(start_pair.2);
+        connection_count += 1;
+        if connection_count >= num_connections {
+            break;
+        }
+        loop {
+            let mut added = 0;
+            for pair in closest_pairs {
+                if circuit.contains(&pair.1) && !circuit.contains(&pair.2) {
+                    circuit.insert(pair.2);
+                    used.insert(pair.2);
+                    added += 1;
+                    connection_count += 1;
+                } else if circuit.contains(&pair.2) && !circuit.contains(&pair.1) {
+                    circuit.insert(pair.1);
+                    used.insert(pair.1);
+                    added += 1;
+                    connection_count += 1;
                 }
-                circuit.insert(pair_add);
-                map.insert(pair_add, idx_use);
-                // eprintln!("map[idx1]: {}, map[idx2]: {}", map[&pair.1], map[&pair.2]);
-                circuits[idx_use] = Some(circuit);
-                // eprintln!("Set circuits[{}] to {:?}", idx_use, circuits[idx_use]);
-                // Also change all other references to dropped circuit to new circuit
-                for (_, old_ref) in map.iter_mut().filter(|(_, c)| **c == idx_drop) {
-                    *old_ref = idx_use;
+                if connection_count >= num_connections {
+                    break 'outer;
                 }
+            }
+            if added == 0 {
+                break;
             }
         }
+        circuits.push(circuit);
     }
-    circuits.sort_by_key(|a| Reverse(a.as_ref().map_or(0, |v| v.len())));
+    circuits.sort_by_key(|c| Reverse(c.len()));
     circuits
 }
 
