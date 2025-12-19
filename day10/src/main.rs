0e535065274b026d50995f3cdc98296d9e7b28bf
Will Train <87541128+wt-314159@users.noreply.github.com>
2025-12-19 17:49:34 +0000

    day10 part 2 first attempt, far too slow
    Need a different approach, can try a different heuristic which favors states closer to goal, or just different algorithm entirely

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -11,12 +11,28 @@ fn main() {
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"#;
 
-    part_one(input);
+    part_two(input);
 }
 
+#[allow(dead_code)]
 fn part_one(input: &str) {
     let machines = input.lines().map(Machine::from);
     let total: usize = machines.map(find_fewest_steps).sum();
+    println!("Total steps: {total}");
+}
+
+fn part_two(input: &str) {
+    let machines = input.lines().map(Machine::from);
+    let total = input.lines().count();
+    let mut count = 0;
+
+    let total: usize = machines
+        .map(|m| {
+            count += 1;
+            eprintln!("Finding steps for machine {}/{}", count, total);
+            find_fewest_jolt_steps(m)
+        })
+        .sum();
     println!("Total steps: {total}");
 }
 
@@ -68,11 +84,60 @@ fn find_fewest_steps(machine: Machine) -> usize {
                     lights: neighbour,
                     cost,
                     steps: current.0.steps + 1,
-                }))
+                }));
             }
         }
     }
     panic!("Didn't find target");
+}
+
+fn find_fewest_jolt_steps(machine: Machine) -> usize {
+    let mut map: HashMap<Vec<usize>, usize> = HashMap::new();
+    let mut heap = BinaryHeap::new();
+
+    let start_state = vec![0; machine.joltages.len()];
+
+    map.insert(start_state.clone(), 0);
+    let cost = joltage_heuristic(&start_state, &machine.joltages, 0);
+    let start_state = JoltState {
+        joltage: start_state,
+        steps: 0,
+        cost,
+    };
+    heap.push(Reverse(start_state));
+
+    while !heap.is_empty() {
+        let current = heap.pop().unwrap();
+        if current.0.joltage == machine.joltages {
+            return current.0.steps;
+        }
+
+        // Iterate over neighbours
+        for button in &machine.buttons {
+            let neighbour = button.apply_jolts(&current.0.joltage);
+            if overjolted(&neighbour, &machine.joltages) {
+                continue;
+            }
+
+            // Check if neighbour already mapped, if not insert
+            let mut new_state = false;
+            let to_enter = neighbour.clone();
+            let entry = map.entry(to_enter).or_insert_with(|| {
+                new_state = true;
+                current.0.steps + 1
+            });
+            if new_state || current.0.steps + 1 < *entry {
+                *entry = current.0.steps + 1;
+                let cost = joltage_heuristic(&neighbour, &machine.joltages, current.0.steps + 1);
+                heap.push(Reverse(JoltState {
+                    joltage: neighbour,
+                    cost,
+                    steps: current.0.steps + 1,
+                }));
+            }
+        }
+    }
+    panic!("Didn't find joltage target!");
 }
 
 #[derive(Clone, Debug)]
@@ -104,6 +169,33 @@ impl PartialEq for State {
 }
 
 impl Eq for State {}
+
+#[derive(Clone, Debug)]
+struct JoltState {
+    joltage: Vec<usize>,
+    cost: usize,
+    steps: usize,
+}
+
+impl PartialOrd for JoltState {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for JoltState {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.cost.cmp(&other.cost)
+    }
+}
+
+impl PartialEq for JoltState {
+    fn eq(&self, other: &Self) -> bool {
+        self.cost == other.cost
+    }
+}
+
+impl Eq for JoltState {}
 
 #[derive(Clone, Debug, PartialEq)]
 struct Machine {
@@ -142,6 +234,14 @@ impl Button {
         let mut new_state = lights.clone();
         for &idx in &self.light_idxs {
             new_state.0[idx] = !new_state.0[idx];
+        }
+        new_state
+    }
+
+    fn apply_jolts(&self, joltage: &[usize]) -> Vec<usize> {
+        let mut new_state = Vec::from(joltage);
+        for &idx in &self.light_idxs {
+            new_state[idx] += 1;
         }
         new_state
     }
@@ -185,6 +285,19 @@ impl From<&str> for Machine {
 
 fn heuristic(state: &Lights, target: &Lights, steps: usize) -> usize {
     4 * steps + state.calc_dist(target)
+}
+
+fn joltage_heuristic(state: &[usize], target: &[usize], steps: usize) -> usize {
+    5 * steps
+        + state
+            .iter()
+            .zip(target.iter())
+            .map(|(&a, b)| b.abs_diff(a))
+            .sum::<usize>()
+}
+
+fn overjolted(state: &[usize], target: &[usize]) -> bool {
+    state.iter().zip(target.iter()).any(|(a, b)| a > b)
 }
 
 impl Lights {
